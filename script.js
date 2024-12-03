24d7140a76d1519a35fdc76e9c8c0c399c14bade
BMDogo <bashirmmuktar017@gmail.com>
2024-12-03 15:33:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+       function showHide(id) {
+   var bash = document.getElementById(id);
+       if (bash.style.display === "none")
+         { 
+           bash.style.display = "block";
+      } else {
+       bash.style.display = "none";
+       }
+       }
+
+
+
